06634b68030cb8b3c746d0d40ed35827921f76fd
Andy Durette <duretteandy@gmail.com>
2020-06-15 14:51:50 -0400

    Gave clarity on battlegrid page

--- pages/battlegrid.js
+++ pages/battlegrid.js
@@ -12,7 +12,10 @@ const Battlegrid = () => {
 			<section id="battlegrid" className="page-content">
 				<div id="battlegrid-message" >
 					<h1>Battle Grid</h1>
-					<p>Here a grid for combat scenarios that uses a server to updates in real time on every browser looking at it. Note however that this feature is only functions correctly on desktop sized screens does not work with touch screen technology. Screens under 1000px wide will see the battlegrid faded out and disabled to interaction.</p>
+					<p>Here a grid for combat scenarios that uses a server to updates in real time on every browser looking at it. Note however that this feature is only functions correctly on desktop sized screens does not work with touch screen technology. Screens under 1000px wide will see the battlegrid faded out and disabled to interaction.
+						<br/><br/>
+						(The battlegrid runs on a server you may need to give it a few moments to boot up)
+					</p>
 				</div>
 				
 				<iframe id="battlegrid-iframe" src="https://draganddropapp.herokuapp.com/"></iframe>
